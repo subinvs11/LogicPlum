3f63e82892e9c13acdae572f4f70a8df53ae4661
subin <subinvs11@gmail.com>
2019-02-24 13:43:40 +0530

    rlease 1

---
+++ graph_generator.py
@@ -0,0 +1,45 @@
+import shap
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+class GraphGenerator:
+
+  def generate_graph(self, file_path):
+    error = ''
+    graph_html = None
+    try:
+      data = pd.read_csv(file_path)
+      x = data.drop('class', axis=1)
+      y = data['class']
+      x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1)
+      clf = RandomForestClassifier(random_state=0).fit(x_train, y_train)
+
+      row_to_show = [51]
+      data_for_prediction = x.iloc[row_to_show]  # use 1 row of data here. Could use multiple rows if desired
+      data_for_prediction_array = data_for_prediction.values.reshape(1, -1)
+      
+      explainer = shap.TreeExplainer(clf)
+      shap_values = explainer.shap_values(data_for_prediction)
+      #shap.initjs()
+      graph_html = shap.force_plot(explainer.expected_value[1], shap_values[1], 
+                      data_for_prediction, plot_cmap=["#f44141","#415bf4"], show=False)
+      
+      sv = explainer.shap_values(x)
+      plt.clf()
+      shap.summary_plot(sv[1], x, plot_type="dot", color_bar="Pkyg", show=False)
+      plt.savefig('static/graphs/graph1.png', bbox_inches="tight")
+      plt.clf()
+      
+      shap.dependence_plot("BloodPressure", sv[1], x, show=False)
+      plt.savefig('static/graphs/graph2.png', bbox_inches="tight")
+      plt.clf()
+      
+      shap.summary_plot(sv[1], x, plot_type="bar", show=False)
+      plt.savefig('static/graphs/graph3.png', bbox_inches="tight")
+    except Exception as e:
+      error = str(e)
+    finally:
+      return graph_html, error
---
+++ index.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, url_for, request
+from werkzeug import secure_filename
+
+from graph_generator import GraphGenerator
+
+app = Flask(__name__)
+UPLOAD_FOLDER = '/uploads' 
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload/', methods = ['GET', 'POST'])
+def upload():
+    error = ''
+    method = 'get'
+    graph_html = None
+    if request.method == 'POST':
+        method = 'post'
+        f = request.files['file']
+        file_name = secure_filename(f.filename)
+        try:
+            extension = file_name.split('.')[1]
+            print ('extension -> ',extension)#zzz
+            if extension.lower() != 'csv':
+                error = 'Invalid file. Please upload a csv file.'
+        except:
+            error = 'Invalid file. Please upload a csv file.'
+        if not error:
+            file_path = 'uploads/' + file_name
+            f.save(file_path)
+            graph_html, error = GraphGenerator().generate_graph(file_path)
+
+    return render_template('upload.html', method=method, 
+                            graph_html=graph_html, error=error)
+    
+
+if __name__ == '__main__':
+   app.run(debug = True)
